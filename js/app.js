ead90fa14eecc119462961ba678746e6e93f5e5c
Bruce Pouncey <brucepouncey@Bruces-MacBook-Pro.local>
2019-10-09 13:57:25 -0400

    style: 1240px polish

--- js/app.js
+++ js/app.js
@@ -3,16 +3,16 @@ const addToCartButton = document.getElementsByTagName("button");
 const notiftext = document.getElementsByClassName("notif-text");
 let cartTotal = 0;
 
-submitButton[0].addEventListener("click", function(event){
+submitButton[0].addEventListener("click", function(){
     alert("Thanks for subscribing!");
-    console.log(event);
 })
 
 for (let i = 0; i < addToCartButton.length; i++) {
     addToCartButton[i].addEventListener("click", function(){
-        cartTotal += 1;
-        notiftext[0].innerHTML = cartTotal;
-        console.log(cartTotal);
+        UpdateCart();
     })
-    
+}
+function UpdateCart() {
+    cartTotal += 1;
+    notiftext[0].innerHTML = cartTotal;
 }
