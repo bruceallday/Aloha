23a30d9857197d476f9d659935e1b7cdad4378b1
Bruce Pouncey <brucepouncey@Bruces-MacBook-Pro.local>
2019-10-09 16:46:42 -0400

    style: Added Initial flickety CDN

--- js/app.js
+++ js/app.js
@@ -16,3 +16,16 @@ function UpdateCart() {
     cartTotal += 1;
     notiftext[0].innerHTML = cartTotal;
 }
+
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'left',
+    contain: true
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity('.main-carousel', {
+    // options
+});
