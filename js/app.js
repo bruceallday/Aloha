fd8a01e23d4a7501c031b6273e52836f7d1ab79e
Bruce Pouncey <brucepouncey@Bruces-MacBook-Pro.local>
2019-10-11 11:18:20 -0400

    feature: Implemented JS Email Validation

--- js/app.js
+++ js/app.js
@@ -9,23 +9,7 @@ document.addEventListener("DOMContentLoaded", function (){
     const updatesLocation = document.getElementsByClassName("updatesLocation");
     let cartTotal = 0;
 
-    pageLinks.forEach(element => {
-        element.addEventListener("click", function(){
-            if(element.id === "updates") {
-                updatesLocation[0].scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
-
-            }else if(element.id === "featured"){
-                featuredLocation[0].scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
-
-            }else if(element.id === "shop"){
-                shopLocation[0].scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
-
-            }else if (element.id === "about"){
-                aboutLocation[0].scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
-            }
-        })     
-    });
-
+    // Flkty set-up
     var elem = document.querySelector('.main-carousel');
     var flkty = new Flickity(elem, {
         cellAlign: 'center',
@@ -35,9 +19,11 @@ document.addEventListener("DOMContentLoaded", function (){
     var flkty = new Flickity('.main-carousel', {
     });
 
-    submitButton[0].addEventListener("click", function () {
-        alert("Thanks for subscribing!");
-    })
+    pageLinks.forEach(element => {
+        element.addEventListener("click", function(){
+            ScrollToPosition(element, updatesLocation, featuredLocation, shopLocation, aboutLocation);
+        })     
+    });
 
     for (let i = 0; i < addToCartButton.length; i++) {
         addToCartButton[i].addEventListener("click", function () {
@@ -48,8 +34,60 @@ document.addEventListener("DOMContentLoaded", function (){
         cartTotal += 1;
         notiftext[0].innerHTML = cartTotal;
     }
-})
 
+    function ScrollToPosition(element, updatesLocation, featuredLocation, shopLocation, aboutLocation) {
+        if (element.id === "updates") {
+            updatesLocation[0].scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        }
+        else if (element.id === "featured") {
+            featuredLocation[0].scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        }
+        else if (element.id === "shop") {
+            shopLocation[0].scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        }
+        else if (element.id === "about") {
+            aboutLocation[0].scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+        }
+    }
+
+    // ----------------------------------------------------------------------------------------------------------------------------------------
+    const emailForm = document.getElementById("email-form");
+
+    emailForm.addEventListener("submit", function () {
+        const textBox = document.getElementById("input");
+        const formData = textBox.value;
+
+        console.log(formData);
+
+        if(IsEmailValid(formData)){
+            alert("Thanks for subscribing!");
+        }else{
+            alert("Please enter a valid email!")
+        }
+    })
+
+    function HasSpecialCharacter(input) {
+        const specialCharacters = ["@", ".com", ".ca", ".co.uk", "gmail", "yahoo"]
+
+        for (let i = 0; i < specialCharacters.length; i++) {
+            if (input.indexOf(specialCharacters[i]) > - 1) {
+                return true;
+            }
+        }
+    }
+
+    function IsEmailValid(input) {
+        if (HasSpecialCharacter(input)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // ----------------------------------------------------------------------------------------------------------------------------------------
+
+
+})
 
 
 
